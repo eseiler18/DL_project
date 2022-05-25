1d871293db4490690b83b3a2836f3fc8a7725182
mannexbcz <79717687+mannexbcz@users.noreply.github.com>
2022-05-25 08:58:44 +0200

    update show

--- Projet2/utils.py
+++ Projet2/utils.py
@@ -19,5 +19,8 @@ def show(imgs):
         im = torchvision.transforms.functional.to_pil_image(im)
         ax[0, i].imshow(np.asarray(im))
         ax[0, i].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
+    ax[0,0].set_title('Noisy')
+    ax[0,1].set_title('Denoised')
+    ax[0,2].set_title('Clean')
     plt.tight_layout()
     plt.show()
